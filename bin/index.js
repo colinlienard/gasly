79c8165d19934ae53d55f9401c2cd433b7083de3
colinlienard <colin.lienard87@orange.fr>
2022-02-24 14:49:40 +0100

    feat(core): add basic questions

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,18 @@
 #! /usr/bin/env node
 
-console.log('Hello world!');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Snippet to use: ', (snippet) => {
+  rl.question('Path: ', (path) => {
+    rl.question('New file name: ', (file) => {
+      console.log(snippet, path, file);
+
+      rl.close();
+    });
+  });
+});
