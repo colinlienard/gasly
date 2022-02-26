3b96aa7500d7df80d0b9be16375d53b40d2453e8
Colin Lienard <colin.lienard87@orange.fr>
2022-02-26 15:50:22 +0100

    feat(core): read file

--- bin/index.js
+++ bin/index.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
-
 const readline = require('readline');
+const fs = require('fs');
 
 const rl = readline.createInterface({
   input: process.stdin,
@@ -11,6 +11,14 @@ rl.question('Snippet to use: ', (snippet) => {
   rl.question('Path: ', (path) => {
     rl.question('New file name: ', (file) => {
       console.log(snippet, path, file);
+
+      fs.readFile(`.gasly/${snippet}`, 'utf8', (error, data) => {
+        if (error) {
+          console.error(error);
+          return;
+        }
+        console.log(data);
+      });
 
       rl.close();
     });
