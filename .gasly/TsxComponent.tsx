511ea05cac33c891dcd881291feef875919a7980
Colin Lienard <64312634+ColinLienard@users.noreply.github.com>
2022-02-27 21:33:30 +0100

    feat(core): add prompts and read/write files (#3)
    * fix: correct CLI keyword
    
    * feat(core): add basic questions
    
    * feat(core): read file
    
    * feat(core): write file and replace its name
    
    * refactor(core): use synchronous `fs` API
    
    * feat(core): add logs to the console for success or fail

---
+++ .gasly/TsxComponent.tsx
@@ -0,0 +1,14 @@
+// @ts-nocheck
+import { FC } from 'react';
+
+type Props = {
+
+};
+
+const TsxComponent: FC<Props> = ({ }) => {
+  return (
+    <></>
+  );
+};
+
+export default TsxComponent;
--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,28 @@
 #! /usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
 
-console.log('Hello world!');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question('Snippet to use: ', (snippet) => {
+  rl.question('Path: ', (filePath) => {
+    rl.question('New file name: ', (file) => {
+      const [snippetName, extension] = snippet.split('.');
+
+      try {
+        const content = fs.readFileSync(`.gasly/${snippet}`, 'utf8');
+        const replacedContent = content.replaceAll(snippetName, file);
+        fs.writeFileSync(`${filePath}/${file}.${extension}`, replacedContent);
+        console.log(`Your file has been created ! Open it by clicking here: ${path.resolve('.')}/${filePath}/${file}.${extension}`);
+      } catch {
+        console.log('An error occured. See the documentation: https://github.com/ColinLienard/gasly#readme');
+      }
+
+      rl.close();
+    });
+  });
+});
