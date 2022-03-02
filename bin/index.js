2cdc5d8949104ccd497c6aeee8c1e81962699831
Colin Lienard <colin.lienard87@orange.fr>
2022-03-02 14:34:24 +0100

    feat(core): show available snippets

--- bin/index.js
+++ bin/index.js
@@ -8,21 +8,36 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+const createFile = (snippet, filePath, file) => {
+  const [snippetName, extension] = snippet.split('.');
+
+  try {
+    const content = fs.readFileSync(`.gasly/${snippet}`, 'utf8');
+    const replacedContent = content.replaceAll(snippetName, file);
+    fs.writeFileSync(`${filePath}/${file}.${extension}`, replacedContent);
+    console.log(`Your file has been created ! Open it by clicking here: ${path.resolve('.')}/${filePath}/${file}.${extension}`);
+  } catch {
+    console.log('An error occured. See the documentation: https://github.com/ColinLienard/gasly#readme');
+  }
+
+  rl.close();
+};
+
+const snippets = fs.readdirSync('.gasly', 'utf8');
+const selected = 0;
+const logSnippets = () => {
+  const choice = snippets.reduce((previous, current, index) => (
+    `${previous}${previous && '\n'}${index === selected ? '>' : ' '} ${current}`
+  ), '');
+  console.log(choice);
+};
+
+logSnippets();
+
 rl.question('Snippet to use: ', (snippet) => {
   rl.question('Path: ', (filePath) => {
     rl.question('New file name: ', (file) => {
-      const [snippetName, extension] = snippet.split('.');
-
-      try {
-        const content = fs.readFileSync(`.gasly/${snippet}`, 'utf8');
-        const replacedContent = content.replaceAll(snippetName, file);
-        fs.writeFileSync(`${filePath}/${file}.${extension}`, replacedContent);
-        console.log(`Your file has been created ! Open it by clicking here: ${path.resolve('.')}/${filePath}/${file}.${extension}`);
-      } catch {
-        console.log('An error occured. See the documentation: https://github.com/ColinLienard/gasly#readme');
-      }
-
-      rl.close();
+      createFile(snippet, filePath, file);
     });
   });
 });
